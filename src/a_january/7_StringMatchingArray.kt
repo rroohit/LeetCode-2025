05b8e288e589ab0705818abb8a9266291adfc8f9
rohitt <crohitp9@gmail.com>
2025-01-07 21:46:38 +0530

    Problem Solved : January   7. String Matching in an Array.

--- src/a_january/7_StringMatchingArray.kt
+++ src/a_january/7_StringMatchingArray.kt
@@ -1,5 +1,6 @@
 package a_january
 
+
 /**
  *  Problem 7. String Matching in an Array.
  *
@@ -45,3 +46,44 @@ private fun weHaveSubString(words: Array<String>, word: String): Boolean {
     }
     return false
 }
+
+private class Solution7 {
+    private data class TrieNode(
+        var frequency: Int = 0, // Tracks how many times this substring appears in the Trie.
+        var childNodes: MutableMap<Char, TrieNode> = HashMap()  // Maps characters to their respective child nodes.
+    )
+
+    fun stringMatching(words: Array<String>): List<String> {
+        val matchingWords: MutableList<String> = ArrayList()
+        val root = TrieNode()
+        for (word in words) {
+            for (startIndex in word.indices) {
+                insertWord(root, word.substring(startIndex))
+            }
+        }
+
+        for (word in words) {
+            if (isSubstring(root, word)) {
+                matchingWords.add(word)
+            }
+        }
+
+        return matchingWords
+    }
+
+    private fun insertWord(root: TrieNode, word: String) {
+        var currentNode: TrieNode? = root
+        for (c in word.toCharArray()) {
+            currentNode!!.childNodes.putIfAbsent(c, TrieNode())
+            currentNode = currentNode.childNodes[c]
+            currentNode!!.frequency++
+        }
+    }
+
+    private fun isSubstring(root: TrieNode, word: String): Boolean {
+        var currentNode: TrieNode? = root
+        for (c in word.toCharArray()) currentNode = currentNode!!.childNodes[c]
+        return currentNode!!.frequency > 1
+    }
+}
+
