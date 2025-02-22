240a09e8f529064875802946a36bae50b6aba812
rohitt <crohitp9@gmail.com>
2025-02-22 09:40:07 +0530

    Problem Solved : February   22. Recover a Tree From Preorder Traversal.

--- src/b_february/22_RecoverTreeFromPreorderTraversal.kt
+++ src/b_february/22_RecoverTreeFromPreorderTraversal.kt
@@ -2,6 +2,8 @@ package b_february
 
 import z_data_type.TreeNode
 import z_data_type.printInorderTree
+import java.util.*
+
 
 /**
  *  Problem 22. Recover a Tree From Preorder Traversal.
@@ -32,11 +34,47 @@ fun main() {
 
 }
 
+fun recoverFromPreorder(traversal: String): TreeNode {
+    val stack = Stack<TreeNode>()
+    var index = 0
+
+    while (index < traversal.length) {
+        var depth = 0
+        while (index < traversal.length && traversal[index] == '-') {
+            depth++
+            index++
+        }
+
+        var value = 0
+        while (index < traversal.length && traversal[index] != '-') {
+            value = value * 10 + traversal[index++].digitToInt()
+        }
+
+        val node = TreeNode(value)
+
+        while (stack.size > depth) stack.pop()
+
+        if (!stack.empty()) {
+            if (stack.peek().left == null) {
+                stack.peek().left = node
+            } else {
+                stack.peek().right = node
+            }
+        }
+
+        stack.push(node)
+    }
+
+    while (stack.size > 1) stack.pop()
+    return stack.peek()
+}
+
 private var ind = 0
-fun recoverFromPreorder(traversal: String): TreeNode? {
+fun recoverFromPreorder1(traversal: String): TreeNode? {
     ind = 0
     return treeConst(traversal, 0)
 }
+
 private fun treeConst(str: String, depth: Int): TreeNode? {
     if (ind >= str.length) return null
 
