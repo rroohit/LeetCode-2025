fbbc9db21e99c6fc8e9cfd8015f4c87c04b96b37
rohitt <crohitp9@gmail.com>
2025-03-27 10:44:39 +0530

    Problem Solved : March   27. Minimum Index of a Valid Split.

---
+++ src/c_march/27_MinimumIndexValidSplit.kt
@@ -0,0 +1,46 @@
+package c_march
+
+/**
+ *  Problem 27. Minimum Index of a Valid Split.
+ *
+ *  ## Intuition -
+ *
+ *  ## Approach -
+ *
+ *  ## Complexity:
+ *       - Time complexity: O()
+ *
+ *       - Space complexity: O()
+ *
+ * ## Code -
+ */
+fun main() {
+
+    val testCases = listOf(
+        listOf(1, 2, 2, 2)
+    )
+
+    testCases.forEach { nums ->
+        println("Result ==> ${minimumIndex(nums)}")
+    }
+
+}
+
+// TC - O(n) :: SC - O(n)
+fun minimumIndex(nums: List<Int>): Int {
+    val first = HashMap<Int, Int>()
+    val second = HashMap<Int, Int>()
+
+    for (num in nums) {
+        second[num] = second.getOrDefault(num, 0) + 1
+    }
+
+    for (i in nums.indices) {
+        val num = nums[i]
+        second[num] = second[num]!! - 1
+        first[num] = first.getOrDefault(num, 0) + 1
+        if (first[num]!! * 2 > i + 1 && second[num]!! * 2 > nums.size - i - 1) return i
+    }
+
+    return -1
+}
