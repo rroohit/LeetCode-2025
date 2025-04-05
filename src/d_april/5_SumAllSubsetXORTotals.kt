9ea27be733f6cf8d37542bb767232a777434d79b
rohitt <crohitp9@gmail.com>
2025-04-05 10:42:05 +0530

    Problem Solved : March   5. Sum of All Subset XOR Totals.

---
+++ src/d_april/5_SumAllSubsetXORTotals.kt
@@ -0,0 +1,38 @@
+package d_april
+
+/**
+ *  Problem 5. Sum of All Subset XOR Totals.
+ *
+ *  ## Intuition -
+ *
+ *  ## Approach -
+ *
+ *  ## Complexity:
+ *       - Time complexity: O(2^n)
+ *
+ *       - Space complexity: O(n)
+ *
+ * ## Code -
+ */
+fun main() {
+
+    val testCases = listOf(
+        intArrayOf(1, 3),
+        intArrayOf(5, 1, 6),
+        intArrayOf(3, 4, 5, 6, 7, 8)
+    )
+
+    testCases.forEach { nums ->
+        println("Result ==> ${subsetXORSum(nums)}")
+    }
+
+}
+
+fun subsetXORSum(nums: IntArray): Int {
+    return dfs(0, nums, 0)
+}
+
+private fun dfs(i: Int, nums: IntArray, total: Int): Int {
+    if (i == nums.size) return total // base case
+    return dfs(i + 1, nums, total xor nums[i]) + dfs(i + 1, nums, total)
+}
