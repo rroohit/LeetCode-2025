99f3599fb9407a3202bc561be2c043b4aae6777e
charlie <crohitp9@gmail.com>
2025-07-09 20:52:50 +0530

    July :  -  9. Reschedule Meetings for Maximum Free Time I.

--- src/g_july/9_RescheduleMeetingsForMaximumFreeTimeI.kt
+++ src/g_july/9_RescheduleMeetingsForMaximumFreeTimeI.kt
@@ -16,6 +16,22 @@ import kotlin.math.max
  *
  * ## Code -
  */
+fun main() {
+
+    val testCases = listOf(
+        Data9(
+            eventTime = 5,
+            k = 1,
+            intArrayOf(1, 3),
+            intArrayOf(2, 5)
+        )
+    )
+
+    testCases.forEach { test ->
+        println("Result ==> ${maxFreeTime(test.eventTime, test.k, test.startTime, test.endTime)}")
+    }
+
+}
 
 fun maxFreeTime(
     eventTime: Int,
@@ -36,4 +52,33 @@ fun maxFreeTime(
         }
     }
     return res
+}
+
+private data class Data9(
+    val eventTime: Int,
+    val k: Int,
+    val startTime: IntArray,
+    val endTime: IntArray
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Data9
+
+        if (eventTime != other.eventTime) return false
+        if (k != other.k) return false
+        if (!startTime.contentEquals(other.startTime)) return false
+        if (!endTime.contentEquals(other.endTime)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = eventTime
+        result = 31 * result + k
+        result = 31 * result + startTime.contentHashCode()
+        result = 31 * result + endTime.contentHashCode()
+        return result
+    }
 }
