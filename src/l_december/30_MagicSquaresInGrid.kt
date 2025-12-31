2a3bc6cfb9c374e905128024f3f77fb6d3f2fb0a
charlie <crohitp9@gmail.com>
2025-12-31 14:37:08 +0530

    December :  - 30 : Magic Squares In Grid.

--- src/l_december/30_MagicSquaresInGrid.kt
+++ src/l_december/30_MagicSquaresInGrid.kt
@@ -49,3 +49,46 @@ private fun isMagicCell(r: Int, c: Int, grid: Array<IntArray>): Boolean {
     if (diagonal1 != diagonal2) return false
     return diagonal1 == 15
 }
+
+class Solution30 {
+    private lateinit var mGrid: Array<IntArray>
+    fun numMagicSquaresInside(grid: Array<IntArray>): Int {
+        mGrid = grid
+        var magicSq = 0
+        val row = mGrid.size
+        val col = mGrid[0].size
+        if (row < 3 || col < 3) return 0
+
+        for (r in 1.. row - 2) {
+            for (c in 1.. col - 2) if (isMagicSq(r, c)) magicSq++
+        }
+
+        return magicSq
+    }
+
+    private fun isMagicSq(r: Int, c: Int) : Boolean {
+        val seen = BooleanArray(10) { false }
+        for (r in r - 1..r + 1) {
+            var rowSum = 0
+            for (c in c - 1 .. c + 1) {
+                val num = mGrid[r][c]
+                if (num !in 1..9 || seen[num]) return false
+                seen[num] = true
+                rowSum += num
+            }
+            if (rowSum != 15) return false
+        }
+
+        val digOne = mGrid[r][c] + mGrid[r - 1][c - 1] + mGrid[r + 1][c + 1]
+        val digTwo = mGrid[r][c] + mGrid[r - 1][c + 1] + mGrid[r + 1][c - 1]
+        if (digOne != 15 || digTwo != 15) return false
+
+        for (c in c - 1..c + 1) {
+            var colSum = 0
+            for (r in r - 1..r + 1) colSum += mGrid[r][c]
+            if (colSum != 15) return false
+        }
+
+        return true
+    }
+}
